8c46df0099a5112dd481d3906ef4af5b252def14
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-03 12:14:10 -0600

    Made UpdatedViewController and made a segue way to UpdateViewScreen

---
+++ TransitionsPrpject/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UpdatedViewController.swift
+//  TransitionsPrpject
+//
+//  Created by Nguyen, Duncan on 10/3/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
